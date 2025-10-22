0e7da552f74da73a7267f5fc7b9fbe575e997256
Mahlet-assefa <mahletassefacodes@gmail.com>
2025-10-22 19:28:35 +0300

    type conversion

---
+++ types/typeConversions.js
@@ -0,0 +1,16 @@
+let value = true 
+alert(typeof(value));
+alert(typeof(String(value)));
+alert("6"/"2");
+let a = "6";
+alert(typeof(a));
+alert(typeof(Number(a)));
+alert(Number(""));
+//results in 0
+alert(Number(true));
+//1
+alert(Number("123z"));
+//NaN
+
+
+
